03d4db2060d76b9eea539996051c2509df54b539
unknown <heartslash2@gmail.com>
2016-04-05 19:35:09 -0400

    Changed for loop

--- src/vitalSigns/VitalSigns.java
+++ src/vitalSigns/VitalSigns.java
@@ -6,7 +6,7 @@ public class VitalSigns {
 		//System.out.println((int)(Math.random()*10));
 		String[] vitals = new String[1000];
 		for (int i = 0; i < 1000; i++) { // start random generator * 1000
-			
+			vitals[i] = Integer.valueOf(i).toString();
 		} // end random generator * 1000
 	} // end main
 
