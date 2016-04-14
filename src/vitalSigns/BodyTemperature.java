e2c0ff4e5f42f02b91ff8b24191233f53d698788
heartslash2 <heartslash2@gmail.com>
2016-04-14 19:14:46 -0400

    Add base BodyTemperature functionality

--- src/vitalSigns/BodyTemperature.java
+++ src/vitalSigns/BodyTemperature.java
@@ -3,9 +3,33 @@ package vitalSigns;
 public class BodyTemperature {
 
 	private float actualBodyTemperature;
+	private float[] validRange;
+	private String status;
 	
 	public BodyTemperature() {
-		this.actualBodyTemperature = (float)Math.random()*10;
+		this(97, 99, 91, 105);
+	}
+	
+	public BodyTemperature(int actualMinimum, int actualMaximum,
+			int validRangeMinimum, int validRangeMaximum) {
+		this.validRange[0] = validRangeMinimum;
+		this.validRange[1] = validRangeMaximum;
+		this.actualBodyTemperature = (int)((Math.random() * (actualMaximum - actualMinimum)) + actualMinimum);
+		isValid();
+	}
+	
+	private void isValid() {
+		// is actualGlucoseLevel in valid range?
+		if (this.actualBodyTemperature > this.validRange[0]
+				&& this.actualBodyTemperature < this.validRange[1]) {
+			setStatus("normal");
+			// is actualGlucoseLevel too low?
+		} else if (this.actualBodyTemperature < this.validRange[0]) {
+			setStatus("low");
+			// is actualGlucoseLevel too high?
+		} else if (this.actualBodyTemperature > this.validRange[1]) {
+			setStatus("high");
+		}
 	}
 	
 	public void setActualBodyTemperature(float actualBodyTemperature) {
@@ -15,5 +39,36 @@ public class BodyTemperature {
 	public float getActualBodyTemperature() {
 		return actualBodyTemperature;
 	}
+	
+	/**
+	 * Setter for <em>validRange</em> by float array
+	 * @param validRange The range of valid heart-rates for this sensor
+	 */
+	public void setValidRange(float[] validRange) {
+		this.validRange = validRange;
+	}
+	
+	/**
+	 * Setter for <em>validRange</em> by float values
+	 * @param minimumValidRange The minimum number of the validRange
+	 * @param maximumValidRange The maximum number of the validRange
+	 */
+	public void setValidRange(float minimumValidRange, float maximumValidRange) {
+		this.validRange[0] = minimumValidRange;
+		this.validRange[1] = maximumValidRange;
+	}
+	
+	public float[] getValidRange() {
+		return validRange;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
 	
 }
