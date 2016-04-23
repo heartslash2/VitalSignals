589d07a806b65d85418f33284ac3880bb2b0c261
heartslash2 <heartslash2@gmail.com>
2016-04-23 18:44:32 -0400

    Format changes. Edit TestClient comments

--- src/vitalSigns/HeartRate.java
+++ src/vitalSigns/HeartRate.java
@@ -1,40 +1,44 @@
 package vitalSigns;
 
 /**
- * The <em>HeartRate</em> class is used to represent a heart-rate sensor. 
+ * The <em>HeartRate</em> class is used to represent a heart-rate sensor.
+ * 
  * @author Jonathan McMeans (mcmeans.4@wright.edu)
  *
  */
 public class HeartRate {
-	
-	/** 
-	 * The <em>actualHeartRate</em> variable used to represent the patient's heart-rate.
+
+	/**
+	 * The <em>actualHeartRate</em> variable used to represent the patient's
+	 * heart-rate.
 	 */
 	private int actualHeartRate;
 	/**
-	 * The <em>normalRange</em> array used to represent the range of normal heart rates for
-	 * this sensor.
+	 * The <em>normalRange</em> array used to represent the range of normal
+	 * heart rates for this sensor.
 	 */
 	private int[] normalRange = new int[2];
 	/**
-	 * The <em>status</em> variable used to represent if the sensor reads that the heart-rate
-	 * is too low, normal, or too high.
+	 * The <em>status</em> variable used to represent if the sensor reads that
+	 * the heart-rate is too low, normal, or too high.
 	 */
 	private String status;
-	
+
 	/**
-	 * Simulate a heart-rate sensor. This creates a heart-rate within the range [30, 120] with a
-	 * normal range of [65, 85]. 
+	 * Simulate a heart-rate sensor. This creates a heart-rate within the range
+	 * [30, 120] with a normal range of [65, 85].
 	 */
 	public HeartRate() {
 		// (low range, high range, low normal, high normal)
 		this(30, 120, 65, 85);
 	}
-	
+
 	/**
-	 * Simulate a heart-rate sensor. This creates a heart rate of <em>heartRate</em> with a 
-	 * normal range of [65, 85].
-	 * @param heartRate The heart-rate that a sensor would display.
+	 * Simulate a heart-rate sensor. This creates a heart rate of
+	 * <em>heartRate</em> with a normal range of [65, 85].
+	 * 
+	 * @param heartRate
+	 *            The heart-rate that a sensor would display.
 	 */
 	public HeartRate(int heartRate) {
 		// set normal range
@@ -44,96 +48,108 @@ public class HeartRate {
 		// check if the input heartRate is normal
 		isNormal();
 	}
-	
+
 	/**
 	 * Simulate a heart-rate sensor by passing in minimum and maximum values for
-	 * a random number generator to create a random number in the range [min,max].<br>
+	 * a random number generator to create a random number in the range
+	 * [min,max].<br>
 	 * Note that this includes both min and max as possible values.
-	 * @param actualMinimum A simulated heart-rate minimum (outside normalRange)
-	 * @param actualMaximum A simulated heart-rate maximum (outside normalRange)
-	 * @param normalRangeMinimum The minimum value of the normal range
-	 * @param normalRangeMaximum The maximum value of the normal range
+	 * 
+	 * @param actualMinimum
+	 *            A simulated heart-rate minimum (outside normalRange)
+	 * @param actualMaximum
+	 *            A simulated heart-rate maximum (outside normalRange)
+	 * @param normalRangeMinimum
+	 *            The minimum value of the normal range
+	 * @param normalRangeMaximum
+	 *            The maximum value of the normal range
 	 */
-	public HeartRate(int actualMinimum, 
-					 int actualMaximum, 
-					 int normalRangeMinimum,
-					 int normalRangeMaximum) {
+	public HeartRate(int actualMinimum, int actualMaximum, int normalRangeMinimum, int normalRangeMaximum) {
 		// set normal range
 		this.normalRange[0] = normalRangeMinimum;
 		this.normalRange[1] = normalRangeMaximum;
-		/* generate random number in range [actualMinimum, actualMaximum]
-		 * the number is generated using the standard formula for ranges, that is
-		 * (rand * (max - min)) + min
-		 * uses Math.random(), which returns double, so cast to int after all calculations
+		/*
+		 * generate random number in range [actualMinimum, actualMaximum] the
+		 * number is generated using the standard formula for ranges, that is
+		 * (rand * (max - min)) + min uses Math.random(), which returns double,
+		 * so cast to int after all calculations
 		 */
-		this.actualHeartRate = (int)((Math.random() * (actualMaximum - actualMinimum)) + actualMinimum);
+		this.actualHeartRate = (int) ((Math.random() * (actualMaximum - actualMinimum)) + actualMinimum);
 		// check if the simulated heartRate is normal
 		isNormal();
 	}
-	
+
 	/**
-	 * This method will determine if the <em>actualHeartRate</em> is inside the <em>normalRange</em> 
-	 * or if the <em>actualHeartRate</em> is too high or low. There is no return; instead, this
-	 * method sets the status value to either "low", "normal", or "high"
+	 * This method will determine if the <em>actualHeartRate</em> is inside the
+	 * <em>normalRange</em> or if the <em>actualHeartRate</em> is too high or
+	 * low. There is no return; instead, this method sets the status value to
+	 * either "low", "normal", or "high"
 	 */
 	private void isNormal() {
 		// is actualHeartRate in normal range?
-		if (this.actualHeartRate >= this.normalRange[0] 
-				&& this.actualHeartRate <= this.normalRange[1]) {
+		if (this.actualHeartRate >= this.normalRange[0] && this.actualHeartRate <= this.normalRange[1]) {
 			setStatus("normal");
-		// is actualHeartRate too low?
+			// is actualHeartRate too low?
 		} else if (this.actualHeartRate < this.normalRange[0]) {
 			setStatus("low");
-		// is actualHeartRate too high?
+			// is actualHeartRate too high?
 		} else if (this.actualHeartRate > this.normalRange[1]) {
 			setStatus("high");
 		}
 	}
-	
+
 	/**
 	 * Setter for <em>actualHeartRate</em>
-	 * @param actualHeartRate The reading of the heart-rate sensor
+	 * 
+	 * @param actualHeartRate
+	 *            The reading of the heart-rate sensor
 	 */
 	public void setActualHeartRate(int actualHeartRate) {
 		this.actualHeartRate = actualHeartRate;
 	}
-	
+
 	/**
 	 * Getter for <em>actualHeartRate</em>
+	 * 
 	 * @return <em>actualHeartRate</em> the reading of the heart-rate sensor
 	 */
 	public int getActualHeartRate() {
 		return actualHeartRate;
 	}
-	
+
 	/**
 	 * Setter for <em>normalRange</em> by float array
-	 * @param normalRange The range of normal heart-rates for this sensor
+	 * 
+	 * @param normalRange
+	 *            The range of normal heart-rates for this sensor
 	 */
 	public void setNormalRange(int[] normalRange) {
 		this.normalRange = normalRange;
 	}
-	
+
 	/**
 	 * Setter for <em>normalRange</em> by float values
-	 * @param minimumNormalRange The minimum number of the normalRange
-	 * @param maximumNormalRange The maximum number of the normalRange
+	 * 
+	 * @param minimumNormalRange
+	 *            The minimum number of the normalRange
+	 * @param maximumNormalRange
+	 *            The maximum number of the normalRange
 	 */
 	public void setNormalRange(int minimumNormalRange, int maximumNormalRange) {
 		this.normalRange[0] = minimumNormalRange;
 		this.normalRange[1] = maximumNormalRange;
 	}
-	
+
 	public int[] getNormalRange() {
 		return normalRange;
 	}
-	
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
+
 	public String getStatus() {
 		return status;
-	}	
+	}
 
 }
--- src/vitalSigns/TestClient.java
+++ src/vitalSigns/TestClient.java
@@ -5,16 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+/**
+ * A testing class used to check server sending properly. This client only reads
+ * in one string from the server. To receive multiple strings, run multiple
+ * times. Only one string is recorded at a time.
+ * 
+ * @author Jonathan McMeans (mcmeans.4@wright.edu)
+ */
 public class TestClient {
 
-	
 	public static void main(String[] args) throws IOException {
 		Socket s = new Socket("localhost", 9090);
 		// buffer for reading from an InputStream
 		BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		// read a single line
 		String answer = input.readLine();
+		// print line to console
 		System.out.println(answer);
+		// close socket to prevent memory leaks
 		s.close();
 	}
 }
