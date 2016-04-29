7cf9c3bd67b71077188bde27d58cebbd597c3a4d
heartslash2 <heartslash2@gmail.com>
2016-04-29 08:40:42 -0400

    BloodOxygen comments

--- src/vitalSigns/BloodOxygen.java
+++ src/vitalSigns/BloodOxygen.java
@@ -1,26 +1,73 @@
 package vitalSigns;
 
+/**
+ * The <em>BloodOxygen</em> class is used to represent a blood oxygen sensor.
+ * This measures the blood oxygen as percentage.
+ * 
+ * @author Jonathan McMeans (mcmeans.4@wright.edu)
+ */
 public class BloodOxygen {
 
+	/**
+	 * The <em>actualBloodOxygen</em> variable used to represent the patient's
+	 * blood oxygen.
+	 */
 	private int actualBloodOxygen;
-	private int[] validRange = new int[2];
+	/**
+	 * The <em>normalRange</em> array used to represent the range of normal
+	 * blood oxygen for this sensor.
+	 */
+	private int[] normalRange = new int[2];
+	/**
+	 * The <em>status</em> variable used to represent if the sensor reads that
+	 * the blood oxygen is too low, normal, or too high.
+	 */
 	private String status;
 
+	/**
+	 * Simulate a blood oxygen sensor. This creates a blood oxygen level in the
+	 * range of [80, 100] with a normal range of [94, 98].
+	 */
 	public BloodOxygen() {
+		// (low range, high range, low normal, high normal)
 		this(80, 100, 94, 98);
 	}
-	
+
+	/**
+	 * Simulate a blood oxygen sensor. This creates a blood oxygen level of
+	 * <em>bloodOxygen</em> with a normal range of [94, 98].
+	 * 
+	 * @param bloodOxygen
+	 *            The blood oxygen level a sensor would read
+	 */
 	public BloodOxygen(int bloodOxygen) {
-		this.validRange[0] = 94;
-		this.validRange[1] = 98;
+		// set normal range
+		this.normalRange[0] = 94;
+		this.normalRange[1] = 98;
 		this.actualBloodOxygen = bloodOxygen;
-		isValid();
+		// check if the input blood oxygen is normal
+		isNormal();
 	}
 
-	public BloodOxygen(int actualMinimum, int actualMaximum, int validRangeMinimum, int validRangeMaximum) {
-		// set valid range
-		this.validRange[0] = validRangeMinimum;
-		this.validRange[1] = validRangeMaximum;
+	/**
+	 * Simulate a blood oxygen sensor by passing in minimum and maximum values
+	 * for a random number generator to create a random number in the range of
+	 * [min, max].<br>
+	 * Note that this includes both min and max as possible values.
+	 * 
+	 * @param actualMinimum
+	 *            A simulated blood oxygen level minimum (outside normalRange)
+	 * @param actualMaximum
+	 *            A simulated blood oxygen level maximum (outside normalRange)
+	 * @param normalRangeMinimum
+	 *            The minimum value of the normal range
+	 * @param normalRangeMaximum
+	 *            The maximum value of the normal range
+	 */
+	public BloodOxygen(int actualMinimum, int actualMaximum, int normalRangeMinimum, int normalRangeMaximum) {
+		// set normal range
+		this.normalRange[0] = normalRangeMinimum;
+		this.normalRange[1] = normalRangeMaximum;
 		/*
 		 * generate random number in range [actualMinimum, actualMaximum] the
 		 * number is generated using the standard formula for ranges, that is
@@ -28,41 +75,97 @@ public class BloodOxygen {
 		 * so cast to float after all calculations
 		 */
 		this.actualBloodOxygen = (int) ((Math.random() * (actualMaximum - actualMinimum)) + actualMinimum);
-		// set status based on validity
-		isValid();
+		// check if the simulated bloodOxygen is normal
+		isNormal();
 	}
 
 	/**
-	 * This method will determine if the <em>actualHeartRate</em> is inside the
-	 * <em>validRange</em> or if the <em>actualHeartRate</em> is too high or
-	 * low. There is no return; instead, this method sets the status value to
+	 * This method will determine if the <em>actualBloodOxygen</em> is inside
+	 * the <em>normalRange</em> or if the <em>actualBloodOxygen</em> is too high
+	 * or low. There is no return; instead, this method sets the status value to
 	 * either "low", "normal", or "high"
 	 */
-	private void isValid() {
-		// is actualHeartRate in valid range?
-		if (this.actualBloodOxygen >= this.validRange[0] && this.actualBloodOxygen <= this.validRange[1]) {
+	private void isNormal() {
+		// is actualBloodOxygen in normal range?
+		if (this.actualBloodOxygen >= this.normalRange[0] && this.actualBloodOxygen <= this.normalRange[1]) {
 			setStatus("normal");
-			// is actualHeartRate too low?
-		} else if (this.actualBloodOxygen < this.validRange[0]) {
+			// is actualBloodOxygen too low?
+		} else if (this.actualBloodOxygen < this.normalRange[0]) {
 			setStatus("low");
-			// is actualHeartRate too high?
-		} else if (this.actualBloodOxygen > this.validRange[1]) {
+			// is actualBloodOxygen too high?
+		} else if (this.actualBloodOxygen > this.normalRange[1]) {
 			setStatus("high");
 		}
 	}
 
+	/**
+	 * Setter for <em>actualBloodOxygen</em>
+	 * 
+	 * @param actualBloodOxygen
+	 *            The reading of the blood oxygen level sensor
+	 */
 	public void setActualBloodOxygen(int actualBloodOxygen) {
 		this.actualBloodOxygen = actualBloodOxygen;
 	}
 
+	/**
+	 * Getter for <em>actualBloodOxygen</em>
+	 * 
+	 * @return <em>actualBloodOxygen</em> the reading of the blood oxygen level
+	 *         sensor
+	 */
 	public int getActualBloodOxygen() {
 		return actualBloodOxygen;
 	}
-	
+
+	/**
+	 * Setter for <em>normalRange</em> by float array
+	 * 
+	 * @param normalRange
+	 *            The range of normal blood oxygen levels for this sensor
+	 */
+	public void setNormalRange(int[] normalRange) {
+		this.normalRange = normalRange;
+	}
+
+	/**
+	 * Setter for <em>normalRange</em> by int values
+	 * 
+	 * @param minimumNormalRange
+	 *            The minimum number of the normalRange
+	 * @param maximumNormalRange
+	 *            The maximum number of the normalRange
+	 */
+	public void setNormalRange(int minimumNormalRange, int maximumNormalRange) {
+		this.normalRange[0] = minimumNormalRange;
+		this.normalRange[1] = maximumNormalRange;
+	}
+
+	/**
+	 * Getter for <em>normalRange</em>
+	 * 
+	 * @return <em>normalRange</em> the range of normal blood oxygen levels for
+	 *         this sensor
+	 */
+	public int[] getNormalRange() {
+		return normalRange;
+	}
+
+	/**
+	 * Setter for <em>status</em>
+	 * 
+	 * @param status
+	 *            the health status normal/low/high
+	 */
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	
+
+	/**
+	 * Getter for <em>status</em>
+	 * 
+	 * @return <em>status</em> the health status normal/low/high
+	 */
 	public String getStatus() {
 		return status;
 	}
